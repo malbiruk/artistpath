03e0bb7b91defa0379e006d062d516bad13d1b1a
malbiruk <2601074@gmail.com>
2025-09-07 16:51:37 +0400

    added cloudflare based caching

--- web/frontend/src/config.js
+++ web/frontend/src/config.js
@@ -1,5 +1,5 @@
 // For testing deployed backend from local frontend
 // const API_BASE_URL = "http://localhost:3050/api";
-const API_BASE_URL = "https://kostiuki.duckdns.org/artistpath/api";
+const API_BASE_URL = "https://artistpath-api.fiyefiyefiye.workers.dev/api";
 
 export { API_BASE_URL };
