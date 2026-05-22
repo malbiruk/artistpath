b1a770952d50308e0a525dcb99cf6f8c06d808a2
malbiruk <2601074@gmail.com>
2026-05-23 02:29:20 +0400

    Point frontend API URL to api.artistpath.cc

--- web/frontend/src/config.js
+++ web/frontend/src/config.js
@@ -1,5 +1,4 @@
-// For testing deployed backend from local frontend
 // const API_BASE_URL = "http://localhost:3050/api";
-const API_BASE_URL = "https://artistpath-api.fiyefiyefiye.workers.dev/api";
+const API_BASE_URL = "https://api.artistpath.cc/api";
 
 export { API_BASE_URL };
