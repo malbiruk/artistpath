e2928e96831bda03a3ed7cdaad9493b9d33d3e4a
malbiruk <2601074@gmail.com>
2026-05-23 02:29:33 +0400

    Remove PyO3 bindings from core crate

--- core/src/string_normalization.rs
+++ core/src/string_normalization.rs
@@ -1,21 +1,10 @@
 use unidecode::unidecode;
 
-#[cfg(feature = "python")]
-use pyo3::prelude::*;
-
-#[cfg_attr(feature = "python", pyfunction)]
 pub fn clean_str(input: &str) -> String {
-    unidecode(input) // Convert Unicode to ASCII
+    unidecode(input)
         .trim()
         .to_lowercase()
         .split_whitespace()
         .collect::<Vec<&str>>()
         .join(" ")
-}
-
-#[cfg(feature = "python")]
-#[pymodule]
-pub fn normalization(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(clean_str, m)?)?;
-    Ok(())
 }
