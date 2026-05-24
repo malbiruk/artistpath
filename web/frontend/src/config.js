e1d596c07ad9c908785a5c63863ee0ce3d78e60d
malbiruk <2601074@gmail.com>
2026-05-25 00:43:46 +0400

    Point frontend at api.artistpath.cc (prod tunnel)

--- web/frontend/src/config.js
+++ web/frontend/src/config.js
@@ -1,4 +1,5 @@
 // const API_BASE_URL = "http://localhost:3050/api";
+// const API_BASE_URL = "https://api-dev.artistpath.cc/api";
 const API_BASE_URL = "https://api.artistpath.cc/api";
 
 export { API_BASE_URL };
